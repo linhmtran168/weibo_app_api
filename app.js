9102774ab4bc7ebd020438e183519141e58980de
Linh Tran <linhtm@icts.vn>
2013-02-06 14:31:46 +0700

    use cluster

--- app.js
+++ app.js
@@ -12,96 +12,109 @@ var express = require('express')
   , passport = require('passport')
   , validator = require('express-validator')
   , flash = require('connect-flash')
+  , cluster = require('cluster')
   , errHandler = require('./configs/errorHandler');
 
-// Configure i18n
-i18n.configure({
-  locales:['en', 'cn', 'jp'],
-});
-
-var app = express();
-
-app.configure(function(){
-  // Base configuration
-  app.set('port', process.env.PORT || 3200);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.bodyParser({
-    keepExtensions: true
-  }));
-
-
-  // Use express-validator
-  app.use(validator);
-  app.use(express.methodOverride());
-
-  // Static file
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  // Express Logger
-  app.use(express.logger('dev'));
-
-  // Session configuration
-  app.all(/^(?!\/api).*$/,express.cookieParser('DragonLinhVoDich'));
-  app.all(/^(?!\/api).*$/, express.session({
-    store: new RedisStore({ db: 'weiboAppSessions', maxAge: 14400000 }),
-    secret: 'DragonLinhVodich'
-  }));
-
-  // Passport initialization
-  app.all(/^(?!\/api).*$/, passport.initialize());
-  app.all(/^(?!\/api).*$/, passport.session());
-
-  // Set up flash message
-  app.all(/^(?!\/api).*$/, flash());
-
-  // CSRf configuration
-  app.all(/^(?!\/api).*$/, express.csrf());
-
-  // Authentication configuration
-  app.all(/^(?!\/api).*$/, function(req, res, next) {
-    // Set the local user = req.user
-    res.locals.currentUser = req.user;
-    return next();
+// Using cluster
+if (cluster.isMaster) {
+  // count the machine's CPUS
+  var cpuCount = require('os').cpus().length;
+  
+  // Create a worker for each CPU
+  var i;
+  for (i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+} else {
+  // Configure i18n
+  i18n.configure({
+    locales:['en', 'cn', 'jp'],
   });
 
-  // Use i18n
-  app.use(i18n.init);
+  var app = express();
 
-  // Configure local variables
-  // i18n in template
-  app.locals({
-    __: i18n.__,
-    __n: i18n.__n
+  app.configure(function(){
+    // Base configuration
+    app.set('port', process.env.PORT || 3200);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.bodyParser({
+      keepExtensions: true
+    }));
+
+
+    // Use express-validator
+    app.use(validator);
+    app.use(express.methodOverride());
+
+    // Static file
+    app.use(require('less-middleware')({ src: __dirname + '/public' }));
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // Express Logger
+    app.use(express.logger('dev'));
+
+    // Session configuration
+    app.all(/^(?!\/api).*$/,express.cookieParser('DragonLinhVoDich'));
+    app.all(/^(?!\/api).*$/, express.session({
+      store: new RedisStore({ db: 'weiboAppSessions', maxAge: 14400000 }),
+      secret: 'DragonLinhVodich'
+    }));
+
+    // Passport initialization
+    app.all(/^(?!\/api).*$/, passport.initialize());
+    app.all(/^(?!\/api).*$/, passport.session());
+
+    // Set up flash message
+    app.all(/^(?!\/api).*$/, flash());
+
+    // CSRf configuration
+    app.all(/^(?!\/api).*$/, express.csrf());
+
+    // Authentication configuration
+    app.all(/^(?!\/api).*$/, function(req, res, next) {
+      // Set the local user = req.user
+      res.locals.currentUser = req.user;
+      return next();
+    });
+
+    // Use i18n
+    app.use(i18n.init);
+
+    // Configure local variables
+    // i18n in template
+    app.locals({
+      __: i18n.__,
+      __n: i18n.__n
+    });
+    app.locals.slug = '';
+
+    // Router configuration
+    app.use(app.router);
+
+    // Error Handler
+    app.use(errHandler.logErrors);
+    app.use(errHandler.clientErrorHandler);
   });
-  app.locals.slug = '';
 
-  // Router configuration
-  app.use(app.router);
+  app.configure('development', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+    mongoose.connect('mongodb://localhost:27017/weiboapp', { user: 'weiboadmin', pass: 'DragonLinhVoDich' });
+  });
 
-  // Error Handler
-  app.use(errHandler.logErrors);
-  app.use(errHandler.clientErrorHandler);
-});
+  app.configure('production', function() {
+    app.use(express.errorHandler());
+    mongoose.connect('mongodb://localhost:27017/weiboapp', { user: 'weiboadmin', pass: 'DragonLinhVoDich' });
+  });
 
-app.configure('development', function() {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  mongoose.connect('mongodb://localhost:27017/weiboapp', { user: 'weiboadmin', pass: 'DragonLinhVoDich' });
-});
+  // Passport configuration
+  require('./configs/passport').config(passport);
 
-app.configure('production', function() {
-  app.use(express.errorHandler());
-  mongoose.connect('mongodb://localhost:27017/weiboapp', { user: 'weiboadmin', pass: 'DragonLinhVoDich' });
-});
+  // Route for the app
+  require('./routes')(app);
 
-// Passport configuration
-require('./configs/passport').config(passport);
-
-// Route for the app
-require('./routes')(app);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port') + ' | Worker ' + cluster.worker.id + ' running');
+  });
+}
