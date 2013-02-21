e4e30546651094604dd427fce9d33f6f76ea2c1c
Linh Tran <linhtm@icts.vn>
2013-02-21 15:17:02 +0700

    add weibo connect to edit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -107,19 +107,17 @@ $(function() {
   /*
    * weibo connect
    */
-  if ($('wb_connect_btn').length > 0) {
-    WB2.anyWhere(function(W){
-    W.widget.connectButton({
-        id: "wb_connect_btn",	
-        type:"3,1",
-        callback : {
-            login:function(o){	//登录后的回调函数
-            },	
-            logout:function(){	//退出后的回调函数
-            }
-        }
-      });
+  WB2.anyWhere(function(W){
+  W.widget.connectButton({
+      id: "wb_connect_btn",	
+      type:"2,1",
+      callback : {
+          login:function(o){	//登录后的回调函数
+          },	
+          logout:function(){	//退出后的回调函数
+          }
+      }
     });
-  }
+  });
 });
 
